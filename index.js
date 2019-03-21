b6f1197693246a48923c30d9a4dc719845b08cb6
github username <github email address>
2019-03-21 12:20:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const testVar = {}
+const testVar = {
+}
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
 
 function testFunc() {
   return "hi"
 }
+
+function split(sentence) {
+  return sentence.split(" ")
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1)
+}
+
+function titleCased(arr) {
+  let splitArr = arr.map(sentence => split(sentence));
+
+  let capArr = splitArr.map(arr => {
+    return arr.map(word => capitalize(word));
+  })
+
+  return capArr.map(sentence => sentence.join(" "));
+}
+let titleCase = titleCased(tutorials)
